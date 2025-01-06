16375d8fa2e69610148c8fcbde7528f48cd36437
Riikka Lilja-Niemi <riikka.lilja@gmail.com>
2025-01-06 22:01:08 +0200

    Added Checkbox to ShoppingListPage

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const H1 = styled.h1`
   color: black;
@@ -17,39 +19,66 @@ const StyledRecipeForm = styled.div`
   margin: 20px;
 `;
 
+const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-decoration: ${({ isChecked }) => (isChecked ? 'line-through' : 'none')};
+  color: ${({ isChecked }) => (isChecked ? '#888' : 'inherit')};
+`;
+
+const List = styled.ul`
+  padding: 0px;
+`;
+
 const ShoppingListPage = ({ shoppingList, removeFromShoppingList, clearShoppingList }) => {
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  const toggleChecked = (item) => {
+    setCheckedItems((prev) =>
+      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
+    );
+  };
+
   return (
     <StyledRecipeForm>
       <H1>Ostoslista</H1>
       {shoppingList.length === 0 ? (
         <p>Ostoslista on tyhjä.</p>
       ) : (
-        <ul>
+        <List>
           {shoppingList.map((item, index) => (
-            <li key={index}>
-              {item}{' '}
+            <ListItem key={index} isChecked={checkedItems.includes(item)}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checkedItems.includes(item)}
+                    onChange={() => toggleChecked(item)}
+                    sx={{ color: 'black', '&.Mui-checked': { color: '#235633' } }}
+                  />
+                }
+                label={item}
+              />
               <IconButton
                 aria-label="poista"
                 size="small"
                 sx={{
                   backgroundColor: 'transparent',
                   '&:hover': {
-                  backgroundColor: 'black',
-              },
-              '&:hover svg': {
-              color: 'white',
-            },
-              marginLeft: '10px',
-            }}
-            onClick={() => removeFromShoppingList(item)}
-          >
-            <CloseIcon fontSize="small" sx={{ color: 'black' }} />
-          </IconButton>
-            </li>
+                    backgroundColor: 'black',
+                  },
+                  '&:hover svg': {
+                    color: 'white',
+                  },
+                }}
+                onClick={() => removeFromShoppingList(item)}
+              >
+                <CloseIcon fontSize="small" sx={{ color: 'black' }} />
+              </IconButton>
+            </ListItem>
           ))}
-        </ul>
+        </List>
       )}
-
       {shoppingList.length > 0 && (
         <Button
           variant="contained"
@@ -63,7 +92,6 @@ const ShoppingListPage = ({ shoppingList, removeFromShoppingList, clearShoppingL
             borderRadius: '30px',
             textTransform: 'none',
             marginTop: '20px',
-            fontFamily: "'Source Code Pro', monospace",
           }}
           onClick={clearShoppingList}
         >
